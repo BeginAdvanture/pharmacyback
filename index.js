3321f777bf2800433d2d0e6f81ac82ab540e94c6
SungHoon77 <ssfu777@gmail.com>
2023-04-21 12:03:04 +0900

    약국 지도1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let express = require("express");
 let axios = require("axios");
 
 let app = express();
-let port = process.env.PORT || 80;
+let port = process.env.PORT || 3000;
 
 app.use(express.static("public_html"));
 app.listen(port, function(){
